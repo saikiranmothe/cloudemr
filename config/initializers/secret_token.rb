22d46863194e50fd300ef413ef49ccfddab988d7
saikiranmothe <saikiran.mothe@gmail.com>
2013-02-28 23:13:22 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloudemr::Application.config.secret_token = 'f1119205fe6b7ffe91c9a5fdcf822f47974fcf823255b56e032f5c1c955421aba59843229715de00439672dbefb9ffe2e08addba66d888bc844566f762d4339f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cloudemr::Application.config.session_store :cookie_store, key: '_cloudemr_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cloudemr::Application.config.session_store :active_record_store
